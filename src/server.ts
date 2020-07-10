4908b6794c133d5c5ef0b0c11aacd68f71adc256
Ed Junior <edinho.029@gmail.com>
2020-07-10 09:27:43 -0300

    Instalação do CORS

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,14 @@ import routes from './routes';
 
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Expose-Headers", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers","GET,POST,PUT");
+  next();
+ });
+
 app.use(express.json());
 app.use(routes);
 
