9cc2f7148dcfe2b29233a03deb5d0cec29b420ad
Kaynan Sampaio <kaynansampaio@MacBook-Pro-de-Kaynan.local>
2020-08-01 13:31:32 -0300

    Adicionando Categoria Controller

---
+++ src/controllers/CategoriaController.ts
@@ -0,0 +1,30 @@
+import {Request,Response} from 'express';
+import knex from '../database/connection';
+
+class CategoriaController{
+  //Criar Categoria
+  async create(request: Request, response: Response){
+    const { descricao } = request.body;
+    
+    const encoutrouCategoria = await knex('tb_Categoria').select('*').first().where({ descricao })
+
+    if (encoutrouCategoria) {
+      return response.status(400).json({ message: 'Já existe uma categoria com essa descrição!'})
+    }
+
+    const categoria = { descricao }
+
+    await knex('tb_Categoria').insert(categoria);
+
+    return response.status(200).json({ message: 'Categoria criada com sucesso'});
+  }
+
+  //Pegar lista com todas categorias¼
+  async listar(request:Request, response:Response){
+    const categorias = await knex('tb_Categoria').select('*');
+
+    return response.json(categorias); 
+  }
+}
+
+export default CategoriaController; 
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import express, { request, response } from "express";
 
+import CategoriaController from './controllers/CategoriaController';
 import ProdutoController from './controllers/ProdutoController';
 import UsuarioController from './controllers/UsuarioController';
 
 const routes = express.Router();
+
+const categoriaController = new CategoriaController();
 const produtoController = new ProdutoController();
-const usuarioController = new UsuarioController
+const usuarioController = new UsuarioController();
 
 routes.get('/usuarios', usuarioController.listar);
 routes.post('/usuarios', usuarioController.create);
@@ -13,6 +16,9 @@ routes.post('/auth', usuarioController.auth);
 
 routes.post('/produtos', produtoController.create);
 routes.get('/produtos', produtoController.listar);
+
+routes.post('/categorias', categoriaController.create);
+routes.get('/categorias', categoriaController.listar);
 
 routes.post('/login',usuarioController.auth);
 
